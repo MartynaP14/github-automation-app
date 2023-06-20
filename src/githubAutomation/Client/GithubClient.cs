6bce010f6ed795b6ed805ba06579cc1a8973ce03
MartynaP14 <martynaxp1906@gmail.com>
2023-06-20 09:53:38 +0100

    client added

---
+++ src/githubAutomation/Client/GithubClient.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace githubAutomation.Client
+{
+    public class GithubClient
+    {
+        private HttpClient _httpClient;
+
+        public GithubClient(HttpClient httpclient)
+        {
+            _httpClient= httpclient;
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "x");
+        }
+            
+
+        //Setting the Bearer token in the Authorisation header 
+        
+
+        //Http request 
+        public async Task<HttpResponseMessage> CreateRepository()
+        {
+           return await _httpClient.GetAsync("");
+        }
+       
+
+
+
+
+
+    }
+}
