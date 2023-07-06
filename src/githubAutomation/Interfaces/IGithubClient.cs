eca99b45c128e58b37cfaeadfe3b958ee7322528
MartynaP14 <martynaxp1906@gmail.com>
2023-07-06 20:18:55 +0100

    interface implemented

---
+++ src/githubAutomation/Interfaces/IGithubClient.cs
@@ -0,0 +1,22 @@
+﻿using githubAutomation.Models;
+using System.Net.Http;
+
+namespace githubAutomation.Interfaces
+{
+    public interface IGithubClient
+    {
+        public Task<HttpResponseMessage> GetRepository(string owner, string repo);
+
+
+
+        public Task<HttpResponseMessage> CreateRepository(Repository repository);
+
+
+
+        public Task<HttpResponseMessage> DeleteRepository(string owner, string repo);
+
+
+        public Task<HttpResponseMessage> UpdateRepository(string owner, string repo, string path, GithubMessage message);
+
+    }
+}
