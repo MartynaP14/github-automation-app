b0fdb2426a462a08d3b8f5137c196a304ebdad56
MartynaP14 <martynaxp1906@gmail.com>
2023-08-07 20:14:19 +0100

    add encoing helper method

---
+++ src/githubAutomation/Services/Base64Encoder.cs
@@ -0,0 +1,21 @@
+﻿using gihubAutomation.Client
+using githubAutomation.Models;
+using System.Text;
+
+namespace githubAutomation.Services
+
+
+{
+    public class Base64Encoder
+    {
+        public string EncodeService(string content)
+        {
+            var headerByte = Encoding.ASCII.GetBytes(content);
+            var headerAsBase64 = Convert.ToBase64String(headerByte);
+            return headerAsBase64;
+        }
+
+
+
+    }
+}
